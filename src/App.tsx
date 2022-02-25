63f4a66ea1123812342aa4d30db9586e7650c32e
TungNS <tungns@vnpay.vn>
2022-02-25 14:49:54 +0700

    test env

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,12 @@ import React from "react";
 import "./App.css";
 import imgUrl from "/src/assets/poster/ghost.png";
 export default function App() {
+  console.log(import.meta.env.VITE_KEY + 1);
+  
   return (
+    <>
+    <div>{import.meta.env.VITE_APP_TITLE}  {import.meta.env.VITE_KEY}</div>
     <img src={imgUrl} alt="nothing" />
+    </>
   );
 }
