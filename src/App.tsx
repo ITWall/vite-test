166ce4faa7637eb4decbd3c15877d56d8e3a374b
TungNS <tungns@vnpay.vn>
2022-02-25 11:51:15 +0700

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Pokemons from "./features/Pokemons";
+import logo from "./logo.svg";
+import "./App.css";
+export default function App() {
+  return (
+    <img src="src/assets/poster/conan.png" alt="nothing" />
+  );
+}
