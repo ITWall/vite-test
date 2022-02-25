a53de743561fef87b19aed259545e3dbd72c6183
TungNS <tungns@vnpay.vn>
2022-02-25 13:37:26 +0700

    test url image

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import imgUrl from "/conan.png";
+import imgUrl from "/src/assets/poster/ghost.png";
 export default function App() {
   return (
     <img src={imgUrl} alt="nothing" />
